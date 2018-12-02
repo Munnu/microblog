94abaa5c8244565d49327a2cf4dbe5432d0b8402
Munnu <moniqueblake4@gmail.com>
2018-12-02 01:57:37 -0800

    Added faux (mock) blog entries

--- app/routes.py
+++ app/routes.py
@@ -7,5 +7,15 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
     # user(left hand side) is a kwarg, user (right hand side) is the variable above
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Home', user=user, posts=posts)
