6b7448ceb156e6c15b56d2e01bca9230b90f5170
Munnu <moniqueblake4@gmail.com>
2018-12-02 15:50:20 -0800

    adding flask environment var configs, specifically SECRET_KEY

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
 
 # imported at the bottom to prevent circular import
 # between routes.py and this file
