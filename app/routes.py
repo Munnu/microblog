aede18e9d32bda43dd332ec928648fe3d2d9e8d6
Munnu <moniqueblake4@gmail.com>
2018-12-02 21:06:55 -0800

    routes contains route for logging in

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 
 # import 'app' variable from the app package (this package)
 from app import app
+
+# import LoginForm from forms.py
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -19,3 +23,19 @@ def index():
     ]
     # user(left hand side) is a kwarg, user (right hand side) is the variable above
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # instantiate a LoginForm object
+    form = LoginForm()
+
+    # Call validate() only if the form is submitted.
+    # This is a shortcut for form.is_submitted() and form.validate()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    # send the instantiated LoginForm object to the view (template)
+    # form (lhs) is the jinja template var name (kwarg), form (rhs) is the variable above
+    return render_template('login.html', title='Sign In', form=form)
