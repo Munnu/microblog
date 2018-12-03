9188d79f9086877a0ad68ecce41f8cb5f1e2e2aa
Munnu <moniqueblake4@gmail.com>
2018-12-03 02:18:46 -0800

    Register a new user to website - functinality

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,8 @@ from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 
 # import 'app' variable from the app package (this package)
-from app import app
+from app import app, db
+from app.forms import RegistrationForm
 
 # import LoginForm from forms.py
 from app.forms import LoginForm
@@ -67,3 +68,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
