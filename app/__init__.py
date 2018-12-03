de63dc2ed5cb522b984a117eb2e94ae2ae07f94f
Munnu <moniqueblake4@gmail.com>
2018-12-02 22:02:28 -0800

    Configuration for SQLAlchemy, creation of models

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # imported at the bottom to prevent circular import
 # between routes.py and this file
-from app import routes
+from app import routes, models
