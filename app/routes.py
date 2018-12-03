186e3635fe4bac41175f3aa21baa793b7ff82a9b
Munnu <moniqueblake4@gmail.com>
2018-12-02 21:33:24 -0800

    Uses url_for() now.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 
 # import 'app' variable from the app package (this package)
 from app import app
@@ -35,7 +35,7 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     # send the instantiated LoginForm object to the view (template)
     # form (lhs) is the jinja template var name (kwarg), form (rhs) is the variable above
     return render_template('login.html', title='Sign In', form=form)
