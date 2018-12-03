63bce1cd279385ef60a1d2f6717925f498d8459b
Munnu <moniqueblake4@gmail.com>
2018-12-02 23:45:33 -0800

    Import and instantiate LoginManager

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 # imported at the bottom to prevent circular import
 # between routes.py and this file
